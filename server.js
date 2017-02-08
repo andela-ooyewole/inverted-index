ed86b599ae298d2f89b36d184df352b35fce4765
Olufemi Oyewole <olufemi.oyewole@andela.com>
2017-02-08 10:55:21 +0100

    chore(server): add express static server

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+// Require express module to serve files for a web browser
 const express = require('express');
 
 const app = express();
 
+// Use express.static middleware function to serve static files from the
+// index.html file located
+app.use(express.static(`${__dirname}/src`));
+
+// Set port to URI on remote server or local host if remote server port is not
+// detected
 app.set('port', (process.env.PORT || 3000));
 
-app.use(express.static(__dirname));
 
 app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
