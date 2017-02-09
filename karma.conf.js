9b73612682f4ca4057515ad6001b1b3afee2ddc3
Olufemi Oyewole <olufemi.oyewole@andela.com>
2017-02-09 15:11:08 +0100

    fix(test-setup): add chrome env variable  for travis

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@
 // Generated on Thu Feb 09 2017 11:55:19 GMT+0100 (WAT)
 
 module.exports = function (config) {
-  config.set({
+  var cfg = {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
@@ -58,6 +58,13 @@ module.exports = function (config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
+    customLaunchers: {
+        Chrome_travis_ci: {
+            base: 'Chrome',
+            flags: ['--no-sandbox']
+        }
+    },
+
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -66,5 +73,11 @@ module.exports = function (config) {
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity
-  })
+  }
+  
+  if (process.env.TRAVIS) {
+      cfg.browsers = ['Chrome_travis_ci'];
+  }
+
+  config.set(cfg)
 }
