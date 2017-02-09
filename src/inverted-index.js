32c14817568ca2fc72c998470e60356a796d44d4
Olufemi Oyewole <olufemi.oyewole@andela.com>
2017-02-09 08:19:50 +0100

    chore(app): update angular development environment

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,28 +1,28 @@
-angular.module('todoApp', [])
-  .controller('TodoListController', function () {
-    const todoList = this;
-    todoList.todos = [
+const app = angular.module('todoApp', []);
+app.controller('TodoListController', () => {
+  const todoList = this;
+  todoList.todos = [
       { text: 'learn AngularJS', done: true },
       { text: 'build an AngularJS app', done: false }];
 
-    todoList.addTodo = function () {
-      todoList.todos.push({ text: todoList.todoText, done: false });
-      todoList.todoText = '';
-    };
+  todoList.addTodo = () => {
+    todoList.todos.push({ text: todoList.todoText, done: false });
+    todoList.todoText = '';
+  };
 
-    todoList.remaining = function () {
-      let count = 0;
-      angular.forEach(todoList.todos, (todo) => {
-        count += todo.done ? 0 : 1;
-      });
-      return count;
-    };
+  todoList.remaining = () => {
+    let count = 0;
+    angular.forEach(todoList.todos, (todo) => {
+      count += todo.done ? 0 : 1;
+    });
+    return count;
+  };
 
-    todoList.archive = function () {
-      const oldTodos = todoList.todos;
-      todoList.todos = [];
-      angular.forEach(oldTodos, (todo) => {
-        if (!todo.done) todoList.todos.push(todo);
-      });
-    };
-  });
+  todoList.archive = () => {
+    const oldTodos = todoList.todos;
+    todoList.todos = [];
+    angular.forEach(oldTodos, (todo) => {
+      if (!todo.done) todoList.todos.push(todo);
+    });
+  };
+});
