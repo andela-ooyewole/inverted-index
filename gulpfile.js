dab41ce448d12955a48a3b55ffe7977c48688666
Olufemi Oyewole <olufemi.oyewole@andela.com>
2017-02-09 10:17:50 +0100

    refactor(devEnv): change default dev port

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 const gulp = require('gulp');
-const bs = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
 
 gulp.task('browser-sync', () => {
-  bs.init({
+  browserSync.init({
     server: {
-      baseDir: 'src',
+      baseDir: './src',
       index: 'index.html'
     },
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 8000,
     ui: false,
     ghostMode: false
   });
