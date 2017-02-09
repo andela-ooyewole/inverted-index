024e5cfcf39816c8d5fb81c2c352b3e570cde8ba
Olufemi Oyewole <olufemi.oyewole@andela.com>
2017-02-09 11:29:06 +0100

    chore(tests-setup): automate test runner refresh

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const browserSyncTests = require('browser-sync').create();
+
+const reload = browserSync.reload;
+const reloadT = browserSyncTests.reload;
 
 gulp.task('browser-sync', () => {
   browserSync.init({
@@ -13,6 +17,30 @@ gulp.task('browser-sync', () => {
   });
 });
 
+gulp.task('browser-sync-tests', () => {
+  browserSyncTests.init({
+    server: {
+      baseDir: ['./src', './jasmine'],
+      index: 'SpecRunner.html'
+    },
+    port: 8888,
+    ui: false,
+    ghostMode: false,
+    // open: false
+  });
+});
+
+gulp.task('watch', ['browser-sync', 'browser-sync-tests'], () => {
+  gulp.watch('src/*.js', reload);
+  gulp.watch('src/*.css', reload);
+  gulp.watch('src/*.html').on('change', reload);
+  gulp.watch(
+    [
+      './src/inverted-index.js',
+      './jasmine/spec/inverted-index-test.js'
+    ], reloadT);
+});
+
 gulp.task('default', [
-  'browser-sync'
+  'browser-sync','browser-sync-tests', 'watch'
 ]);
