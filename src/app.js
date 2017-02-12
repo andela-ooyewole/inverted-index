80a79759d59ec9da53c32fda93db36b470873d7c
Olufemi Oyewole <olufemi.oyewole@andela.com>
2017-02-13 00:43:29 +0100

    feature(file-upload): add file upload functionality

--- src/app.js
+++ src/app.js
@@ -4,34 +4,73 @@ app.config(($mdThemingProvider) => {
     .primaryPalette('light-blue')
     .accentPalette('amber');
 });
-app.controller('MyController', ($scope) => {
+app.controller('MyController', ($scope, $mdDialog) => {
   const index = new Index();
+  $scope.errorMessage = {
+    title: '',
+    content: ''
+  };
+  $scope.fileNames = [];
+  $scope.noFilesSelected = true;
   $scope.query = '';
   $scope.selectedFile = '--select file--';
-  $scope.storeFile = [];
+  $scope.storedFiles = [];
   $scope.search = () => {
     $scope.result = $scope.query;
   };
-  $scope.storeFile = () => {
-    $scope.result = 'working';
-    // index.selectedBooks.push();
+  // display error messages
+  $scope.showAlert = (ev) => {
+    $mdDialog.show(
+      $mdDialog.alert()
+        .parent(angular.element(document.querySelector('#popupContainer')))
+        .clickOutsideToClose(true)
+        .title($scope.errorMessage.title)
+        .textContent($scope.errorMessage.content)
+        .ariaLabel('Alert Dialog')
+        .ok('Got it!')
+        .targetEvent(ev)
+    );
   };
+  // trigger file selection for input
   $scope.upload = () => {
     document.getElementById('input').click();
   };
-  $scope.file_changed = (element) => {
+  // store uploaded files
+  $scope.storeFiles = (element) => {
+    $scope.noFilesSelected = false;
     $scope.$apply(() => {
-      const file = element.files[0];
-      $scope.result = file.name;
-      const reader = new FileReader();
-      reader.readAsText(file);
-      reader.onload = (e) => {
-        // handle onload
-        const fileDetails = e.target.result;
-        const jsonFile = JSON.parse(fileDetails);
-        $scope.contents = jsonFile;
-        // $scope.result = file.name;
-      };
+      for (let i = 0; i < element.files.length; i += 1) {
+        const file = element.files[i];
+        const reader = new FileReader();
+        if ($scope.fileNames.includes(file.name)) {
+          $scope.errorMessage.title = 'File already added';
+          $scope.errorMessage.content = 'Select a new file';
+          $scope.showAlert();
+        } else {
+          reader.readAsText(file);
+          reader.onload = (e) => {
+            // handle onload
+            const name = file.name;
+            const fileContent = e.target.result;
+            const content = JSON.parse(fileContent);
+            const fileValidity = index.validateFile(content);
+            if (fileValidity === 'Valid file') {
+              const fileInformation = {
+                name,
+                content
+              };
+              $scope.storedFiles.push(fileInformation);
+              $scope.fileNames.push(name);
+            } else {
+              $scope.errorMessage.title = fileValidity;
+              $scope.errorMessage.content = `'${name}' is not 
+                a valid JSON file.`;
+              $scope.showAlert();
+              $scope.noFilesSelected = $scope.storedFiles.length === 0;
+            }
+          };
+        }
+      }
     });
   };
 });
